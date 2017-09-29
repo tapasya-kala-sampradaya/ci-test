924e63d9875b9614c3bdd5af2a582d12f9b41626
vipranarayan14 <vipranarayan14@gmail.com>
2017-09-29 20:17:37 +0530

    test commit 15

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 file_deploy() {
-  curl -u $FTP_USER:$FTP_PASS -X DELETE "$FTP_HOST/ci-test/";
   find rama -type f -exec curl --ftp-create-dirs -u $FTP_USER:$FTP_PASS -T {} $FTP_HOST/ci-test/{} \;
 }
 
