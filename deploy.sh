7a3ea3186b290b5fe760d74ca1e2eb70f0102e5c
vipranarayan14 <vipranarayan14@gmail.com>
2017-09-29 19:16:43 +0530

    test commit 13

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 file_deploy() {
-  rm -rf rama;
-  mkdir rama;
-  touch rama/sita.txt;
-  find rama -type f -exec curl --ftp-create-dirs -u $FTP_USER:$FTP_PASS -T {} -Q "rm -rf ci-test" $FTP_HOST/ci-test/{} \;
+
+  find rama -type f -exec curl --ftp-create-dirs -u $FTP_USER:$FTP_PASS -T {} -Q "rm -rf ci-test; mkdir ci-test; cd ci-test" $FTP_HOST/{} \;
 }
 
 os_deploy() {
