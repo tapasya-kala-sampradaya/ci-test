8d4994ca08dfb851dbaf5892ba7029e90f05d101
vipranarayan14 <vipranarayan14@gmail.com>
2017-09-30 17:47:15 +0530

    test changing files 7

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 file_deploy() {
 
-  lftp -u $FTP_USER,$FTP_PASS -e "set ftp:ssl-allow no ; mirror -v -c -e -R ./dist ~/ci-test/ ; exit" -p 21 $FTP_HOST
+  lftp -u $FTP_USER,$FTP_PASS -e "set ftp:ssl-allow no ; mirror -v -c -e -R ./dist ~/ci-test ; exit" -p 21 $FTP_HOST
 }
 
 os_deploy() {
