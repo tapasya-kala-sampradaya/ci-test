bd588c0a1f7ca31c591c836c799e693ba4efea84
vipranarayan14 <vipranarayan14@gmail.com>
2017-09-30 18:58:29 +0530

    test branch push

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,23 @@
 file_deploy() {
+  
+  WORKING_DIR="./dist";
 
-  lftp -u $FTP_USER,$FTP_PASS -e "set ftp:ssl-allow no ; mirror -v -c -e -R ./dist ~/ci-test ; exit" -p 21 $FTP_HOST
+  if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi;
+
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+    REMOTE_DIR="~/ci-test";
+  elif [ "$TRAVIS_BRANCH" == "develop" ]; then
+    REMOTE_DIR="~/ci-test-alpha";
+  else 
+    echo "This will not deploy!";
+    exit 0;
+  fi
+
+  lftp -u $FTP_USER,$FTP_PASS -e "set ftp:ssl-allow no ; mirror -v -c -e -R $WORKING_DIR $REMOTE_DIR ; exit" -p 21 $FTP_HOST
 }
 
 os_deploy() {
-  echo "OS deploy"
+  
+  npm install
+  node script.js
 }
